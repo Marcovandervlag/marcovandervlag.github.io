d765da5a4f9b7b31a6f7e10a14adbb34aab0b127
Marco van der Vlag <107155973+Marcovandervlag@users.noreply.github.com>
2023-02-05 14:29:15 +0100

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -6,19 +6,19 @@ $(function () {
     bar += '<div id="cssmenu">'
         bar += '<ul>'
             bar += '<li>'
-                bar += '<a href="https://koalateen.github.io/oldindex">Home</a>'
+                bar += '<a href="https://marcovandervlag.github.io/oldindex">Home</a>'
 bar += '                '
             bar += '</li>'
             bar += '<li><a>About me<i class="arrow"></i></a>'
                 bar += '<ul class="dropdown">'
-                    bar += '<li><a href="https://koalateen.github.io/wie-ben-ik">Wie ben ik</a></li>'
+                    bar += '<li><a href="https://marcovandervlag.github.io/wie-ben-ik">Wie ben ik</a></li>'
                     bar += '<li>'
                         bar += '<a>Certificaten<i class="arrow"></i></a>'
                         bar += '<ul class="dropdown">'
-                            bar += '<li><a href="https://koalateen.github.io/pythoncert">Python</a></li>'
-                            bar += '<li><a href="https://koalateen.github.io/javacert">Java</a></li>'
-                            bar += '<li><a href="https://koalateen.github.io/communicatie">Communicatie</a></li>'
-                            bar += '<li><a href="https://koalateen.github.io/htmlcert">HTML</a></li>'
+                            bar += '<li><a href="https://marcovandervlag.github.io/pythoncert">Python</a></li>'
+                            bar += '<li><a href="https://marcovandervlag.github.io/javacert">Java</a></li>'
+                            bar += '<li><a href="https://marcovandervlag.github.io/communicatie">Communicatie</a></li>'
+                            bar += '<li><a href="https://marcovandervlag.github.io/htmlcert">HTML</a></li>'
                             
                         bar += '</ul>'
                     bar += '</li>'
@@ -30,19 +30,19 @@ bar += '    '
                 
                         
                 
-                        bar += '<li><a href="https://koalateen.github.io/qr-code-generator-logo">Qr-code generator logo</a></li>'
-                        bar += '<li><a href="https://koalateen.github.io/weather-trough-gps">weather-trough-gps</a></li>'
-                        bar += '<li><a href="https://koalateen.github.io/vraagjes">Vraag voor bij de thee</a></li>'
+                        bar += '<li><a href="https://marcovandervlag.github.io/qr-code-generator-logo">Qr-code generator logo</a></li>'
+                        bar += '<li><a href="https://marcovandervlag.github.io/weather-trough-gps">weather-trough-gps</a></li>'
+                        bar += '<li><a href="https://marcovandervlag.github.io/vraagjes">Vraag voor bij de thee</a></li>'
 
                         
-                        bar += '<li><a href="https://koalateen.github.io/websiteprojects">Website projects</a></li>'
+                        bar += '<li><a href="https://marcovandervlag.github.io/websiteprojects">Website projects</a></li>'
                         bar += '</ul>'
                     
 
             bar += '</li>'
             
             bar += '<li>'
-                bar += '<li><a href="https://koalateen.github.io/contact">Contact</a></li>'
+                bar += '<li><a href="https://marcovandervlag.github.io/contact">Contact</a></li>'
                 bar += '<ul class="dropdown right0">'
                     
                     
